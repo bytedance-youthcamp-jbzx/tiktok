f49b91328a224b2a46940ed51f86637154cdcd99
0202zc <lzc980202@163.com>
2023-02-22 19:06:26 +0800

    修复返回提示不一致的问题

--- cmd/relation/service/handler.go
+++ cmd/relation/service/handler.go
@@ -80,7 +80,7 @@ func (s *RelationServiceImpl) RelationFollowList(ctx context.Context, req *relat
 		logger.Errorf("当前登录用户%d无法访问其他用户的关注列表%d", claims.Id, userID)
 		res := &relation.RelationFollowListResponse{
 			StatusCode: -1,
-			StatusMsg:  "当前登录用户无法访问其他用户的粉丝列表",
+			StatusMsg:  "当前登录用户无法访问其他用户的关注列表",
 		}
 		return res, nil
 	}
@@ -269,7 +269,7 @@ func (s *RelationServiceImpl) RelationFriendList(ctx context.Context, req *relat
 		return res, nil
 	}
 	if userID != claims.Id {
-		logger.Errorf("当前登录用户%d无法访问其他用户的粉丝列表%d", claims.Id, userID)
+		logger.Errorf("当前登录用户%d无法访问其他用户的朋友列表%d", claims.Id, userID)
 		res := &relation.RelationFriendListResponse{
 			StatusCode: -1,
 			StatusMsg:  "当前登录用户无法访问其他用户的朋友列表",
